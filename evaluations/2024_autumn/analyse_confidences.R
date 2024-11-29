fd3b681e24e2962a4982af5776543bf3e90376a8
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-11-29 11:48:44 +0100

    Before cleaning up the data

--- evaluations/2024_autumn/analyse_confidences.R
+++ evaluations/2024_autumn/analyse_confidences.R
@@ -1,26 +1,73 @@
 #!/bin/env Rscript
 
-days <- c("monday", "tuesday", "wednesday", "thursday", "friday")
+get_all_days <- function() {
+  c("monday", "tuesday", "wednesday", "thursday", "friday")
+}
+
+days <- get_all_days()
+testthat::expect_true("monday" %in% get_all_days())
+
+day_to_filename <- function(day) {
+  testthat::expect_true(day %in% get_all_days())
+  paste0("confidences_", day, ".csv")
+}
+
+testthat::expect_equal(day_to_filename("monday"), "confidences_monday.csv")
+
+# Check if all files exist
 for (day in days) {
-  filename <- paste0("confidences_", day, ".csv")
+  filename <- day_to_filename(day)
   testthat::expect_true(file.exists(filename))
 }
 
-# Take the raw string from a CSV file and convert it tp clean header names
-header_to_names <- function(header) {
-  header_strs_raw <- stringr::str_split(string = header, pattern = ",")[[1]]
-  header_strs_1 <- stringr::str_remove(header_strs_raw, "I am confidant that...: ")
-  header_strs_2 <- stringr::str_remove(header_strs_1, "^- ")
-  names <- header_strs_2
-  names
+get_example_header_monday <- function() {
+  "Date,Session,Voter,How confident do you feel about the learning today:,I feel confident I can: The purpouse of formalism in Computer Science,I feel confident I can: What is the Software development life cycle,I feel confident I can: What are the outcomes of a Program,I feel confident I can: What are the steps of development,I feel confident I can: What is UML,I feel confident I can: I know basic Git,I feel confident I can: I can connect to the course repo,I feel confident I can: I kow how a remote repo works,I feel confident I can: I kow why we specify requirments,I feel confident I can: I know ehat a Requirment is ,I feel confident I can: I can find the needs/buissnes requirments,I feel confident I can: I know what a derived requirement,I feel confident I can: I know how to write a formal requiremnts documentation,I feel confident I can: I can do a simple FMEA,I feel confident I can: I can make an issue in the project,I feel confident I can: I know the difference between a requirment and an Feature"  
 }
-
-get_example_header <- function() {
+get_example_header_tuesday <- function() {
+  "Date,Session,Voter,Let's see how confident you are for tuesday:,How confident am I?: I understand what SDLC is,How confident am I?: I know the major parts of a developel cycle,How confident am I?: I understand the spiral model of SDLC,How confident am I?: I know the major technichal risks with a SW project,How confident am I?: I know how to  make a risk for a requirment,How confident am I?: I understand the key features of  XP (eXtreme Programming),How confident am I?: I know the basic features of RAD (Rapid Application Development) ,\"How confident am I?: I know where to find the \"\"latest\"\" best practices\",How comfortable am I with?:  I understand why issues are important,\"How comfortable am I with?: - I can create, assign and close an issue\",How comfortable am I with?: - I understand why a kanban board is important,How comfortable am I with?: - I can use a kanban board,How comfortable am I with?: - I can name at least three roles that are present in a team,How comfortable am I with?: - I understand why TDD is important,How comfortable am I with?: - I can use TDD to develop functions,How comfortable am I with?: - I understand why `assert` is important"
+}
+get_example_header_wednesday <- function() {
+  "Date,Session,Voter,Confidence Report wednesday:,I feel confident that :: - I understand why pair programming is important,I feel confident that :: - I understand what good pair programming looks like,I feel confident that :: - I can pair program,I feel confident that :: - I understand the benefit of using a testing framework,\"I feel confident that :: - I can put the files of my code in the right locations,\",I feel confident that ::   for the testing framework to find them,I feel confident that :: - I can write tests using a formal testing framework,I feel confident that :: - I understand what continuous integration is,confidence level: I can explain and evaluate the usefulness of branches,\"confidence level: I can create, switch and delete a git branch locally\",\"confidence level: create, switch and delete a git branch on GitHub\",confidence level: I can merge locally,confidence level: I can manage merge conflicts locally,confidence level: I can merge on GitHub,confidence level: I can manage merge conflicts on GitHub,Confidence level: I can do Pull requests,Confidence level: I can do a code review (in GitHub),Confidence level: I understand when a forked workflow is recommended,Confidence level: I have an idea about how to contribute as an external contributor,Confidence level: I understand the FIND concept,Confidence level: I feel confidence and motivation in making my code open-source"
+}
+get_example_header_thursday <- function() {
+  "Date,Session,Voter,Its time for our confidence:,What are my confidence level in my...: - Understand the benefits of object-oriented development,\"What are my confidence level in my...:     - Learn the definition and characteristics of an object, including its ability to save state and perform operations.\",\"What are my confidence level in my...:     - Comprehend the principles of object-oriented development, such as information hiding, encapsulation, and message passing.\",What are my confidence level in my...:     - Understand the definition and structure of a class as a collection of objects with common traits and behaviors.,\"What are my confidence level in my...:     - Recognize the importance of relationships between classes, including association, composition, and aggregation, in class diagrams.\",What are my confidence level in my...: I understand why function design is important,What are my confidence level in my...: - I can give some features of good function design,What are my confidence level in my...: - I can give a function a proper name,What are my confidence level in my...: - I can criticise functions,What are my confidence level in my...: - I can write a class to express what the data it contains,What are my confidence level in my...: - I inderstand difference between composition and aggregation,What are my confidence level in my...: - I understand the type of relation 'composition' is,What are my confidence level in my...: - I understand the type of relation 'inheritance' is,What are my confidence level in my...: Understand the concept of code smells and design smells.,What are my confidence level in my...:     Gain an introductory understanding of modular programming,\"What are my confidence level in my...:     Learn about common interfaces, protocols, and their role in modular programming.\",What are my confidence level in my...: Recognize and address tight coupling in code to improve modularity.,What are my confidence level in my...: Iterative refactoring."
+}
+get_example_header_friday <- function() {
   "Date,Session,Voter,Final confidence report:,I am confidant that...:  I understand how to methodically optimize the runtime speed of my code,I am confidant that...: - I understand what Big O is,I am confidant that...: - I can create a Big O profile of my project,I am confidant that...: - I can read a runtime speed profile,I am confidant that...: - I can create a runtime speed profile,I am confidant that...: - I can mentalize the installation needs from the users' perspective,I am confidant that...: - I can evaluate different available tools for reproducibility and installations,I am confidant that...: - I can make an installation instruction for potential users,I am confidant that...: - I know the most important sections for a full public README,I am confidant that...: - I can make citation info,I am confidant that...: - I know how to find instruction of going to more sophisticated documentation,I am confidant that...: The course met my expectations,I am confidant that...: I learned a lot of new things about software development as a process,I am confidant that...: I will use what I learned in my projects"  
 }
 
-get_example_header
 
+# Take the raw string from a CSV file and convert it tp clean header names
+header_to_names <- function(header) {
+  
+  header_strs_as_table <- read.csv(textConnection(header), header = FALSE)
+  header_strs <- as.character(header_strs_as_table[1, ])
+  # testthat::expect_equal(stringr::str_count(header_strs, "\""), 0)
+  remove_strs <- c(
+    "I am confidant that...: ", 
+    "I feel confident I can: ", 
+    "How comfortable am I with\\?: ",
+    "I feel confident that :: ",
+    "Confidence level: ",
+    "confidence level: ",
+    "^- " # Must be last
+  )
+  for (remove_str in remove_strs) {
+    header_strs <- stringr::str_remove(header_strs, remove_str)
+  }
+  header_strs
+}
+
+testthat::expect_equal(c("A", "B"), header_to_names(header = "A,B"))
+
+testthat::expect_equal(0, sum(stringr::str_count(header_to_names(get_example_header_monday()), "I feel")))
+testthat::expect_equal(0, sum(stringr::str_count(header_to_names(get_example_header_tuesday()), "How comfortable")))
+testthat::expect_equal(0, sum(stringr::str_count(header_to_names(get_example_header_wednesday()), "confident")))
+testthat::expect_equal(0, sum(stringr::str_count(header_to_names(get_example_header_wednesday()), "confident")))
+header_to_names(get_example_header_wednesday())
+
+
+testthat::expect_equal(18, length(header_to_names(get_example_header_friday())))
 
 # Convert a confidences file to a table
 file_to_table <- function(filename) {
@@ -37,6 +84,7 @@ file_to_table <- function(filename) {
   names(t) <- table_names
   t
 }
+
 
 t <- readr::read_csv("20240925_fixed.csv")
 t$Timestamp <- NULL
