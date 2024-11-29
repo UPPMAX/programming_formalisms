af8b852732120253ba0397a62df4250e0cbe1773
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-11-29 08:46:39 +0100

    Can check schedule

---
+++ scripts/check_schedule.R
@@ -0,0 +1,94 @@
+#!/bin/env Rscript
+#
+# Checks if the schedule follows all constraints
+#
+# Usage:
+#
+#   ./scripts/check_schedule.R
+
+
+if (stringr::str_count(string = getwd(), pattern = "scripts")) {
+  setwd("..")
+}
+
+if (stringr::str_count(string = getwd(), pattern = "scripts")) {
+  stop(
+    "FATAL ERROR. \n",
+    "Please run the script from the project root. \n",
+    "Present working director: ", getwd(), " \n",
+     " \n",
+     "Tip: like this \n",
+     " \n",
+     "  ./scripts/check_schedule.R \n",
+     " \n"
+  )
+}
+
+get_schedule_filename <- function() {
+  schedule_filename <- "docs/schedule.md"
+  if (!file.exists(schedule_filename)) {
+    stop(
+      "Cannot find '", schedule_filename, "'. \n",
+      "Current working directory: ", getwd()
+    )
+  }
+  schedule_filename
+}
+
+
+read_schedule_from_file <- function() {
+  schedule_filename <- get_schedule_filename()
+  testthat::expect_true(file.exists(schedule_filename))
+  all_lines <- readr::read_lines(file = schedule_filename)
+  schedule_lines_with_header <- stringr::str_subset(all_lines, pattern = ".+\\|.+\\|.+\\|")
+  header_line <- schedule_lines_with_header[1]
+  schedule_lines <- schedule_lines_with_header[-(1:2)]
+  schedule_strs <- stringr::str_split_fixed(schedule_lines, "\\|", n = 4)
+  t_schedule <- tibble::as_tibble(schedule_strs)
+  testthat::expect_equal(ncol(t_schedule), 4)
+  names(t_schedule) = c("day","time","teacher","subject")
+  t_schedule$day <- stringr::str_trim(t_schedule$day)
+  t_schedule$time <- stringr::str_trim(t_schedule$time)
+  t_schedule$teacher <- stringr::str_trim(t_schedule$teacher)
+  t_schedule$subject <- stringr::str_trim(t_schedule$subject)
+  t_schedule
+}
+
+
+
+create_constraints <- function() {
+  tibble::tribble(
+    ~topic, ~prerequisite,
+    "TDD", "assert"
+  )
+}
+
+# Will stop with an error if not
+check_constraints <- function() {
+  schedule <- read_schedule_from_file()
+  constraints <- create_constraints()
+  for (constraint_index in seq_len(nrow(constraints))) {
+    constraint <- constraints[constraint_index, ]
+    topic <- constraint$topic
+    topic_indices <- stringr::str_which(schedule$subject, pattern = topic)
+    # A topic may be taught multiple hours
+    topic_index <- topic_indices[1]
+    testthat::expect_true(topic_index > 0)
+    
+    prerequisite <- constraint$prerequisite
+    prereq_indices <- stringr::str_which(schedule$subject, pattern = prerequisite)
+    # A prereq may be taught multiple hours
+    prereq_index <- prereq_indices[1]
+    testthat::expect_true(prereq_index > 0)
+    
+    if (prereq_index > topic_index) {
+      stop(
+        "Incorrect order detected: \n",
+        "Topic: ", topic, " at row ", topic_index, " \n",
+        "Prerequisite: ", prerequisite, " at row ", prereq_index, " \n"
+      )
+    }
+  }
+}
+
+check_constraints()
