3a16ab6464a316500fb3ca56553270a93c83a183
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-05-08 08:49:53 +0200

    Update

--- scripts/schedule_day_4.sh
+++ scripts/schedule_day_4.sh
@@ -3,9 +3,12 @@
 # Play reminders for the course schedule.
 
 echo 'espeak -s 120 -p 10 "start to work"' | at 9:00
-echo 'espeak -s 120 -p 10 "breaktime"' | at 10:00
-echo 'espeak -s 120 -p 10 "back to work"' | at 10:15
-echo 'espeak -s 120 -p 10 "breaktime"' | at 11:00
+
+# Usually 10:00
+echo 'espeak -s 120 -p 10 "breaktime"' | at 9:45
+
+# echo 'espeak -s 120 -p 10 "back to work"' | at 10:15
+# echo 'espeak -s 120 -p 10 "breaktime"' | at 11:00
 echo 'espeak -s 120 -p 10 "back to work"' | at 11:15	
 echo 'espeak -s 120 -p 10 "lunchtime"' | at 12:00
 echo 'espeak -s 120 -p 10 "start to work"' | at 13:00
